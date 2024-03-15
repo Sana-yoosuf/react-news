c6a1f774897ee3cf2729eaa52e5e7012544cea41
Sana Yoosuf <sanazaier777@gmail.com>
2024-03-15 15:07:07 +0400

    Designing the news app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import News from './News';
 
 function App() {
-  useEffect(()=>{
-    fetch("https://newsapi.org/v2/everything?q=microsoft&from=2024-03-13&apiKey=1583f7cca9134acea33080d819e86625")
-    .then((response)=>response.json())
-    .then((news)=>{
-      console.log(news.articles)
-    })
-  },[])
+  // let [articles,setArticles]=useState([]);
+
+
+
+  // useEffect(()=>{
+  //   fetch("https://newsapi.org/v2/everything?q=india&from=2024-03-13&apiKey=1583f7cca9134acea33080d819e86625")
+  //   .then((response)=>response.json())
+  //   .then((news)=>{
+  //     setArticles(news.articles)
+  //   })
+  // },[])
 
   
 
@@ -20,7 +25,19 @@ function App() {
 
   return (
     <div className="App">
-      hello
+      <header className='header'>
+        <h1>Now Times</h1>
+        <input type='text' placeholder=' Search News'/>
+      </header>
+      <section className='news-articles'>
+
+      
+      <News/>
+      <News/>
+      <News/>
+      <News/>
+      <News/>
+      </section>
     </div>
   );
 }
---
+++ src/News.jsx
@@ -0,0 +1,13 @@
+function News() {
+  return (
+  <div className="news">
+    <div className="news-img">
+
+    </div>
+    <h1>Title</h1>
+    <p>Dummy text with news</p>
+    <a href="#">Read More</a>
+  </div>
+  )
+}
+export default News;
