2fad0a5c8aff81f80d94f4e9a72c77bdc2a81c9b
Sana Yoosuf <sanazaier777@gmail.com>
2024-03-26 21:24:28 +0400

    Generated news

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
-
-import { useEffect, useState } from 'react';
-import './App.css';
-import News from './News';
+import { useEffect, useState } from "react";
+import "./App.css";
+import News from "./News";
 
 function App() {
-  // let [articles,setArticles]=useState([]);
+  let [articles, setArticles] = useState([]);
 
+  useEffect(() => {
+    fetch(
+      "https://newsapi.org/v2/everything?q=india&from=2024-03-13&apiKey=1583f7cca9134acea33080d819e86625"
+    )
+      .then((response) => response.json())
+      .then((news) => {
+        setArticles(news.articles);
+        console.log(news.articles);
+      });
+  }, []);
 
+  // At first then(watever data we are fetching from api we get that as response
+  // and we convert them as json data)
 
-  // useEffect(()=>{
-  //   fetch("https://newsapi.org/v2/everything?q=india&from=2024-03-13&apiKey=1583f7cca9134acea33080d819e86625")
-  //   .then((response)=>response.json())
-  //   .then((news)=>{
-  //     setArticles(news.articles)
-  //   })
-  // },[])
-
-  
-
-// At first then(watever data we are fetching from api we get that as response
-// and we convert them as json data)
-
-// At second then(We are printing that data)
+  // At second then(We are printing that data)
 
   return (
     <div className="App">
-      <header className='header'>
+      <header className="header">
         <h1>Now Times</h1>
-        <input type='text' placeholder=' Search News'/>
+        <input type="text" placeholder=" Search News" />
       </header>
-      <section className='news-articles'>
-
-      
-      <News/>
-      <News/>
-      <News/>
-      <News/>
-      <News/>
+      <section className="news-articles">
+        {articles.map((article) => {
+          return <News article={article}/>;
+        })}
       </section>
     </div>
   );
--- src/News.jsx
+++ src/News.jsx
@@ -1,13 +1,26 @@
-function News() {
+function News(props) {
+  console.log(props);
   return (
-  <div className="news">
-    <div className="news-img">
+    <div className="news">
+      <div className="news-img">
+        <img src={props.article.urlToImage} />
+      </div>
+      <h1>{props.article.title}</h1>
+      <p>
+        {props.article.description?.substring(0, 100).concat("...")}{" "}
+        <a href={props.article.url}>Read More</a>
+      </p>
 
+      {/* substring is js method used to limit the lenght of a paragraph. 
+      In this case we use a null checking operator(?)it is because we 
+      load our data from api so in initial stage it may take some time 
+      to load the data, So ? ignores if there is no data and shortens 
+      the paragraph if there is data */}
+
+      <div className="source">
+        <p>Author : {props.article.author}</p>
+      </div>
     </div>
-    <h1>Title</h1>
-    <p>Dummy text with news</p>
-    <a href="#">Read More</a>
-  </div>
-  )
+  );
 }
 export default News;
